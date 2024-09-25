8fba927776bad1098ffa02096135f0704c91e6ec
StevenSchonlau <sschonla@purdue.edu>
2024-09-24 20:12:10 -0400

    Adding starting spring boot initialization and dependencies

---
+++ YouDash/src/test/java/com/Group26/YouDash/YouDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group26.YouDash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
