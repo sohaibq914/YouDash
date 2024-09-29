826bec630a9cde48fb9363621e56cf9ff1b31cec
sohaibq914 <sohaibq914@gmail.com>
2024-09-29 13:25:40 -0400

    attempt to add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import BlockedPage from "./Pages/BlockedPages.tsx";
-import GoalCreate from "./Pages/GoalCreate"
+import GoalCreate from "./Pages/GoalCreate";
+import Navbar from "./components/navbar.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -29,6 +30,7 @@ function App() {
         <Routes>
           <Route path="/block-categories" element={<BlockedPage />} />
           <Route path="/goals" element={<GoalCreate />} />
+          <Route path="/navbar" element={<Navbar />} />
         </Routes>
 
         <p>{message}</p>
--- frontend/src/Pages/GoalView.js
+++ frontend/src/Pages/GoalView.js
@@ -1,28 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import GoalComponent from "../Components/GoalComponent";
+import GoalComponent from "../components/GoalComponent";
 import "./GoalView.css";
 
 function GoalView() {
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios
-            .get("http://localhost:8080/goals/thename/view")
-            .then(function (response) {
-                document.getElementById("Response").innerHTML = "Name: " + response.data.User + " The Goal: " + response.data.GoalName;
-            })
-            .catch((error) => console.error(error));
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get("http://localhost:8080/goals/thename/view")
+      .then(function (response) {
+        document.getElementById("Response").innerHTML = "Name: " + response.data.User + " The Goal: " + response.data.GoalName;
+      })
+      .catch((error) => console.error(error));
+  };
   return (
     <div className="GoalView">
+      <button style={{ width: "100%" }} type="submit" onClick={handleSubmit}>
+        View Goals
+      </button>
 
-        <button style={{width: "100%"}} type="submit" onClick={handleSubmit}>
-            View Goals
-        </button>
-
-        <p id="Response">NO RESPONSE</p>
-
+      <p id="Response">NO RESPONSE</p>
     </div>
   );
 }
