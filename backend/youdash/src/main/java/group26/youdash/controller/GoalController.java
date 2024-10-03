10bdd2658f81e6645cc19172e1d459b2e09763ec
Steven <sschonla@purdue.edu>
2024-10-03 11:00:16 -0400

    Goal Edit UI and Backend

--- backend/youdash/src/main/java/group26/youdash/controller/GoalController.java
+++ backend/youdash/src/main/java/group26/youdash/controller/GoalController.java
@@ -32,7 +32,7 @@ public class GoalController {
         header.add("200", "uno");
         System.out.println("Received New Goal");
         if (wtgoal.getGoalName().equals("DUP")) {
-
+            //can't have duplicate names
         }
         temp.add(wtgoal);
         return new ResponseEntity<>(header, HttpStatus.OK);
@@ -41,14 +41,39 @@ public class GoalController {
     @GetMapping("/{user}/view")
     public ArrayList<WatchTimeGoal> getName(@PathVariable("user") String user)
     {
-        //Map<String, String>
-        //Map<String, String> ret = new HashMap<>();
-        //ret.put("User", user);
-        //ret.put("GoalName", "This is a goal!");
-        //JSON.;
         System.out.println(user + "Goal View Requested");
         System.out.println(temp);
         return temp;
+    }
+
+    private static class updateGoalPackage {
+        public String originalName;
+        public WatchTimeGoal wtg;
+
+        public updateGoalPackage(String originalName, WatchTimeGoal wtg) {
+            this.originalName = originalName;
+            this.wtg = wtg;
+        }
+    }
+
+    //mapping for "/goals/user/create" that creates a goal based on json
+    @PostMapping(path = "/{user}/edit", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> editGoal(@PathVariable("user") String user, @RequestBody updateGoalPackage wtgoalPkg) {
+        System.out.println(wtgoalPkg);
+        //Update goal based on name
+        for (int i = 0; i < temp.size(); i++) {
+            if (temp.get(i).getGoalName().equals(wtgoalPkg.originalName)) {
+                temp.set(i, wtgoalPkg.wtg);
+                //replace in database
+                break;
+            }
+        }
+
+        HttpHeaders header = new HttpHeaders();
+        header.add("200", "uno");
+        System.out.println("Updated New Goal");
+        System.out.println(temp);
+        return new ResponseEntity<>(header, HttpStatus.OK);
     }
 
 }
--- frontend/src/Pages/GoalEdit.js
+++ frontend/src/Pages/GoalEdit.js
@@ -1,11 +1,45 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./GoalEdit.css";
+import GoalEditComponent from "../Components/GoalEditComponent"
 
 function GoalEdit() {
+
+const [data, setData] = useState([]);
+    const addGoals = (newGoals) => {
+        let dataTemp = [];
+        for (var i=0; i <newGoals.length; i++) {
+            dataTemp.push(newGoals[i]);
+        }
+        setData(dataTemp);
+    };
+    const clearData = () => {
+        setData([]);
+    };
+    useEffect (() => {
+        axios
+            .get("http://localhost:8080/goals/thename/view")
+            .then(function (response) {
+                clearData();
+
+                addGoals(response.data);
+            })
+            .catch((error) => console.error(error));
+    }, []);
+
   return (
     <div className="GoalEdit">
       <h1>In Progress</h1>
+        {data.map((igoal, index) => (
+            <div key={index}>
+                <h2>Goal #{index + 1}</h2>
+                <GoalEditComponent goal={igoal} />
+            </div>
+            ))}
+
+
+
+
     </div>
   );
 }
