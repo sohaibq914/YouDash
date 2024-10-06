eaef02ed51d72cbe694351188b4ef53f284c28a2
Sohaib <106452965+sohaibq914@users.noreply.github.com>
2024-10-06 18:34:22 -0400

    made navbar polished and responsive

--- frontend/src/Components/navbar.js
+++ frontend/src/Components/navbar.js
@@ -1,27 +1,30 @@
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import BlockedPage from "../Pages/BlockedPages.tsx";
-import GoalCreate from "../Pages/GoalCreate.js";
 
 function NavbarComponent() {
   return (
-    <Navbar className="bg-body-tertiary fixed-top">
-      <div className="container-fluid">
+    <Navbar className="bg-body-tertiary fixed-top" expand="md">
+      <Container>
         <Navbar.Brand href="/home">YouDash</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="/leaderboards">Leaderboards</Nav.Link>
-          <NavDropdown title="Goals" id="goal-dropdown">
-            <NavDropdown.Item href="/goalsView">View</NavDropdown.Item>
-            <NavDropdown.Item href="/goalsCreate">Create</NavDropdown.Item>
-            <NavDropdown.Item href="/goalsEdit">Edit</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="/analytics">Analytics</Nav.Link>
-          <Nav.Link href="/block-categories">Blocked Categories</Nav.Link>
-        </Nav>
-      </div>
+        {/* Add Toggle button for responsive menu */}
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/leaderboards">Leaderboards</Nav.Link>
+            <NavDropdown title="Goals" id="goal-dropdown">
+              <NavDropdown.Item href="/goalsView">View</NavDropdown.Item>
+              <NavDropdown.Item href="/goalsCreate">Create</NavDropdown.Item>
+              <NavDropdown.Item href="/goalsEdit">Edit</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/analytics">Analytics</Nav.Link>
+            <Nav.Link href="/block-categories">Blocked Categories</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
