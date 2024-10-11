1386441ee6539873a864b4518fc352cfa3beca4a
Steven <sschonla@purdue.edu>
2024-10-11 09:22:01 -0400

    Fixed Login nav

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
+
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+
+    const handleSignup = (e) => {
+        navigate("/signup");
+    }
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -46,6 +51,10 @@ function Login() {
         </div>
         <button type="submit">Login</button>
       </form>
+      <br/>
+
+      <button onClick={handleSignup}>Sign Up</button>
+
     </div>
   );
 }
--- frontend/src/RouterPages.js
+++ frontend/src/RouterPages.js
@@ -8,6 +8,8 @@ import Home from "./Pages/Home";
 // import Navbar from "./Components/navbar.js";
 import Profile from "./Pages/Profile.js";
 import WatchHistory from "./Pages/WatchHistory.tsx";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { Routes, Route, Link } from "react-router-dom"; // Remove Router and only import Routes and Route
@@ -25,6 +27,8 @@ function RouterPages() {
         <Route path="/goalsView" element={<GoalView />} />
         <Route path="/goalsEdit" element={<GoalEdit />} />
         <Route path="/goalsVis" element={<VisualizeGoal />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         {/* <Route path="/navbar" element={<Navbar />} /> */}
         <Route path="/profile" element={<Profile />} />
         <Route path="/watch-history" element={<WatchHistory />} />
