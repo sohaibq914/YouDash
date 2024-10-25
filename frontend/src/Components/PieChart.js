b2dcc642422c241e2c86fa19324723b3ef0775e0
Steven <sschonla@purdue.edu>
2024-10-25 15:02:53 -0400

    Working Pie Chart

--- frontend/src/Components/PieChart.js
+++ frontend/src/Components/PieChart.js
@@ -8,12 +8,34 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import "./PieChart.css";
 ChartJS.register(ArcElement, Tooltip, Legend);
 function PieChart() {
+    const [percent, setPercent] = useState(0);
+
+
+    useEffect (() => {
+        axios
+            .get("http://localhost:8080/goals/" + getUser() + "/pie")
+            .then(function (response) {
+                setPercent(response.data);
+            })
+            .catch((error) => console.error(error));
+    }, []);
+
+    const getUser = () => {
+                let theUrl = window.location.href;
+                console.log(theUrl);
+                if (theUrl.indexOf("/", theUrl.indexOf("/", 10) + 1) == -1) {
+                    return null;
+                }
+                console.log(theUrl.substring(theUrl.indexOf("/", 10) + 1, theUrl.indexOf("/", theUrl.indexOf("/", 10) + 1)));
+                return theUrl.substring(theUrl.indexOf("/", 10) + 1, theUrl.indexOf("/", theUrl.indexOf("/", 10) + 1));
+
+            }
 
     const data = {
-                       labels: ["Watch", "Avoid"],
+                       labels: ["Progress", "To Go"],
                        datasets: [
                          {
-                           data: [12.4, 77.8],
+                           data: [100*percent, 100*(1 - percent)],
                            backgroundColor: [
                              'rgba(255, 99, 132, 0.8)',
                              'rgba(54, 162, 235, 0.8)'
