7f23423ea3ecb3293e493828046c8a7e0ae24f0c
josep5673 <111795024+josep5673@users.noreply.github.com>
2024-11-04 20:57:52 -0500

    added classes for messages and groups

---
+++ backend/youdash/src/main/java/group26/youdash/classes/Messages.java
@@ -0,0 +1,45 @@
+package group26.youdash.classes;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+
+public class Messages {
+    private String messageId;
+    private String userId;
+    private String messageText;
+    private String timeStamp;
+
+    @DynamoDBAttribute(attributeName = "message_id")
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId =  messageId;
+    }
+
+    @DynamoDBAttribute(attributeName = "user_id")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "message_text")
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    @DynamoDBAttribute(attributeName = "timestamp")
+    public String getTimestamp() {
+        return timeStamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timeStamp = timestamp;
+    }
+}
---
+++ backend/youdash/src/main/java/group26/youdash/model/Groups.java
@@ -0,0 +1,64 @@
+package group26.youdash.model;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+
+import group26.youdash.classes.Messages;
+import java.util.List;
+import java.util.Map;
+
+
+@DynamoDBTable(tableName = "Groups")
+public class Groups {
+    private String groupId;
+    private String groupName;
+    // managers is int because users are ints in our user table e.g. user 12345
+    private List<Integer> managers;
+    private List<Integer> users;
+    private List<Messages> messages;
+
+
+    // Partition key
+    @DynamoDBHashKey(attributeName = "group_id")
+    public String getGroupId() {
+        return groupId;
+    }    
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    @DynamoDBAttribute(attributeName = "group_name")
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @DynamoDBAttribute(attributeName = "manager_list")
+    public List<Integer> getManagers() {
+        return managers;
+    }
+
+    public void setManagers(List<Integer> managers) {
+        this.managers = managers;
+    }
+
+    @DynamoDBAttribute(attributeName = "users")
+    public List<Integer> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Integer> users) {
+        this.users = users;
+    }
+
+    @DynamoDBAttribute(attributeName = "messages")
+    public List<Messages> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Messages> messages) {
+        this.messages = messages;
+    }
+ }
